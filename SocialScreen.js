fdd5b2f67cc93a8968e8418231724f58bb1fa03b
Zachary Primus <zprimus@mail.usf.edu>
2018-10-20 03:32:33 -0400

    Delete SocialScreen.js

--- SocialScreen.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {Button, StyleSheet, Text, View, Dimensions } from 'react-native';
-
-const SocialScreen = () => (
-  <View style={{ backgroundColor: '#673ab7' }} />
-);
-
-export default SocialScreen;
