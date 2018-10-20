2756fde780350417bc03bbf9b5481f31c4d6126a
Zachary Primus <zprimus@mail.usf.edu>
2018-10-20 03:31:45 -0400

    Delete MapScreen.js

--- MapScreen.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {Button, StyleSheet, Text, View, Dimensions } from 'react-native';
-
-const MapScreen = () => (
-  <View style={{ backgroundColor: '#000000' }} />
-);
-
-export default MapScreen;
