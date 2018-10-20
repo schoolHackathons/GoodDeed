590adb4a97d36739cc3ca4d1ddb7eca7d7358ae5
Pranav Sharma <vanarps@gmail.com>
2018-10-20 03:18:03 -0400

    Add files via upload

--- ProfileScreen.js
+++ ProfileScreen.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+//import ProfileScreen from './components/ProfileScreen';
 
-const ProfileScreen = () => {
+export default class ProfileScreen extends React.Component {
+  render() {
     return (
+      <View style={styles.container}>
         <View>
             <View style={styles.top}>
             </View>
             <View style={styles.pictureholder}>
-                {/*<Image source={require('./download.jpg')} style={styles.picture}/>*/}
                 <Ionicons name='md-person' size={150} color='#66ffa6'/>
             </View>
             <View style={styles.nameholder}>
@@ -20,49 +22,52 @@ const ProfileScreen = () => {
             <View style={styles.bottom}>
             </View>
         </View>
+      </View>
     );
+  }
 }
 
 const styles = StyleSheet.create({
-    pictureholder: {
-        flex: 2,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    nameholder: {
-        flex: 1,
-        backgroundColor: '#00b248',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    top: {
-        height: 75,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    bottom: {
-        height: 100,
-        backgroundColor: '#00b248',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    picture: {
-        width: 200,
-        height: 200,
-    },
-    name: {
-        color: '#66ffa6',
-        fontSize: 100,
-    },
-    othertext: {
-        color: '#66ffa6',
-        fontSize: 30,
-    }
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  pictureholder: {
+      flex: 2,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+  },
+  nameholder: {
+      flex: 1,
+      backgroundColor: '#00b248',
+      alignItems: 'center',
+      justifyContent: 'center',
+  },
+  top: {
+      height: 75,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+  },
+  bottom: {
+      height: 100,
+      backgroundColor: '#00b248',
+      alignItems: 'center',
+      justifyContent: 'center',
+  },
+  picture: {
+      width: 200,
+      height: 200,
+  },
+  name: {
+      color: '#66ffa6',
+      fontSize: 100,
+  },
+  othertext: {
+      color: '#66ffa6',
+      fontSize: 30
+  }
 });
-// light green: 66ffa6
-// green: 00e676
-// dark green: 00b248
-
-export default ProfileScreen;
