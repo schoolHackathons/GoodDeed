319131bcaec6c23a5ca557459e0c2a4968e581e0
ApprovedCargo91 <43579092+ApprovedCargo91@users.noreply.github.com>
2018-10-20 21:38:37 -0400

    Added location field, rounded card corners

--- src/SocialRect.js
+++ src/SocialRect.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image} from 'react-native';
+
 class SocialRect extends React.Component { 
   render() {
-    let pic = {
-      uri: 'https://i.imgur.com/xFqtpPf.jpg'
+    let pinPic = {
+      uri: 'https://cdn1.iconfinder.com/data/icons/mirrored-twins-icon-set-hollow/512/PixelKit_point_marker_icon.png'
     }; 
     return(
       <View style={styles.container}>
         <View style={styles.header}>
           <Image source={this.props.PP} style={{width: 50,height: 50}}/>
-          <Text style={{margin: 10}}>{this.props.US}</Text>
+          <Text style={{margin: 10,fontSize: 20,fontWeight: '300'}}>{this.props.US}</Text>
         </View>
         <View style={styles.picture}>
           <Image source={this.props.PIC} style={{width: '100%',minHeight:350,resizeMode:'cover'}}/>
         </View>
         <View style={styles.footer}>
-          <Text>{this.props.DESC}</Text>
+          <Text style={{flex: 1}}>{this.props.DESC}</Text>
+          <View style={styles.location}>
+            <Image source={pinPic} style={{width: 25,height: 25}}/>
+            <Text style={{marginLeft:5,marginRight: 20}}>{this.props.LOC}</Text>
+          </View>
         </View>
       </View>  
     )
@@ -42,9 +47,15 @@ const styles = StyleSheet.create({
   },
   footer: {
     //flex: 1,
-    backgroundColor: '#ffffff',
-    margin: 10,
+    //backgroundColor: '#ffffff',
+    marginVertical: 5,
+    marginHorizontal: 10,
   },
+  location: {
+    flex: 1,
+    marginTop:5,
+    flexDirection:'row',
+  }
 });
 
 export default SocialRect;
